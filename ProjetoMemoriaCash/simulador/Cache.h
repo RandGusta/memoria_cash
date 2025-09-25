550d6c29f06be889335ede65321f4000bb70002e
gusta <gustavorandiiii@gmail.com>
2025-09-25 11:22:59 -0300

    atualizando Cache

--- ProjetoMemoriaCash/simulador/Cache.h
+++ ProjetoMemoriaCash/simulador/Cache.h
@@ -34,19 +34,27 @@ public:
 
         private:
             int associatividade_;
-            int tamanhoLinha_; // valor fixo atribuido no .cpp --> perguntar para o Eduardo
+            int tamanhoLinha_; // valor fixo atribuido no  --> perguntar para o Eduardo
             int tamanhoConjuntoAssociativo_;
             PoliticaDeEscrita politicaDeEscrita_;
-            Memoria* proximoNivel_;
-            std::vector<std::vector<LinhaCache>> cache_;
+            Memoria* proximoNivel_; // para guardar um endereço de um objeto do tipo memoória ou que herde de memória 
+            std::vector<std::vector<LinhaCache>> cache_; // vetor de vetores --> primeiro indice = conjunto da cache segundo indice = linha 
+                                                        // conjunto 0 --> [linha0, linha1, ...]
+
             std:: vector<std::list<unsigned int>> lista_LRU; // LRU --> usada menos recente --> politica de substituição 
                                                             // oque foi usado a MAIS tempo sai para um novo dado entrar
+                                                            // conjunto 0 --> [0, 1...] 
+                                                            // primeiro numero na lista foi o ultimo usado
+                                                            // quando acessar uma linha mandamos ela para o final da lista 
+                                                            // list --> mais rápido 
+
             int leitura_ = 0;
             int escrita_ = 0;
-            int acerto_ = 0; // hit
-            int erro_ = 0; // miss
+            int acerto_ = 0; // hit --> esta na cache
+            int erro_ = 0; // miss --> não esta na cache --> buscar o proximo nível da memória
 
 
+         
         
 };
 
